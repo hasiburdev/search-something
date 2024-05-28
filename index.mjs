f5724bac8d025a5831689ec5a3c2f903b1e9a892
Hasibur Rahman <hasiburdev@gmail.com>
2024-05-28 22:59:05 +0600

    feat: search puppeteer on duckduckgo

--- index.mjs
+++ index.mjs
@@ -9,7 +9,26 @@ const main = async () => {
 
   const page = await browser.newPage();
 
-  await page.goto("duckduckgo.com");
+  await page.goto("https://duckduckgo.com/");
+
+  await page.waitForSelector("#searchbox_input");
+
+  await page.type("#searchbox_input", "puppeteer", {
+    delay: 100,
+  });
+
+  await page.keyboard.press("Enter");
+
+  await page.waitForSelector(".react-results--main", { timeout: 10000 });
+
+  const titles = await page.evaluate(() => {
+    const elements = document.querySelectorAll(
+      "[data-testid='result-title-a']"
+    );
+    return Array.from(elements).map((element) => element.textContent);
+  });
+
+  console.log(titles);
 
   await browser.close();
 };
